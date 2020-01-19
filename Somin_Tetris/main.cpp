ba5cf5d38ed27c34e3e7854ea772889a9b0ad2cd
Somin Daniel <danya1203s@gmail.com>
2020-01-19 16:19:44 +0200

    Spelling bug fixed.

--- Somin_Tetris/main.cpp
+++ Somin_Tetris/main.cpp
@@ -14,7 +14,7 @@ int main()
 
 	//Window creation
 	sf::RenderWindow mainWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), 
-		"Somin Tetris: best clone ever!");
+		"Somin Tetris: the best clone ever!");
 
 	sf::Image icon;
 	icon.loadFromFile("Img\\icon.png"); // File/Image/Pixel
